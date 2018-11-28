f1143e47ca111d5bac06dc9a6562f2e7a68c06c6
Daniel Rogers <daniel.rogers@enable.com>
2018-11-28 12:50:29 +0000

    Add migrate step to startup

--- src/TaskApi/Startup.cs
+++ src/TaskApi/Startup.cs
@@ -55,12 +55,15 @@ namespace TaskApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // TODO - review if this is the desired approach
+            context.Database.Migrate();
 
             // TODO : make this more restrictive
             app.UseCors(builder => builder
